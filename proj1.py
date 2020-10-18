ced65194b6791b9a01655a28870bac56b237a947
mkmak <maixunle@gmail.com>
2020-10-17 17:58:26 -0700

    Add files via upload

---
+++ proj1.py
@@ -0,0 +1,17 @@
+# Hard coding Jonny Likes data
+feature_vecs = [["Circle", "Thick", "Gray", "Thick", "Dark"], #ex1
+                ["Circle", "Thick", "White", "Thick", "Dark"], #ex2
+                ["Triangle", "Thick", "White", "Thick", "Gray"], #ex3
+                ["Circle", "Thin", "White", "Thin", "Dark"], #ex4
+                ["Square", "Thick", "Dark", "Thin", "White"], #ex5
+                ["Circle", "Thick", "White", "Thin", "Dark"], #ex6
+                ["Circle", "Thick", "Gray", "Thick", "White"], #ex7
+                ["Square", "Thick", "White", "Thick", "Gray"], #ex8
+                ["Triangle", "Thin", "Gray", "Thin", "Dark"], #ex9
+                ["Circle", "Thick", "Dark", "Thin", "White"], #ex10
+                ["Square", "Thick", "White", "Thick", "Dark"], #ex11
+                ["Triangle", "Thick", "White", "Thick", "Gray"]] #ex12
+classes = [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0] # 1==pos; 0==neg
+
+print(feature_vecs)
+print(classes)
